2864dfaf966f4798249b1ed41468ba732415feb5
EA - Miguel Da Silva Oliveira <miguel_silva02@educadventista.org>
2023-11-06 15:26:30 -0300

    Recebendo informacoes no corpo da requisicão

--- index.js
+++ index.js
@@ -5,6 +5,29 @@ const app = express()
 
 const caminhoBase = path.join(__dirname, "templates")
 
+app.use(express.urlencod -ed({
+    extended: true
+}))
+
+
+app.use(express.json())
+
+
+app.post('/cadastrar/salvar', (requisicao, resposta) =>{
+    console.log(requisicao.body)
+
+
+})
+
+
+app.get('/cadastrar',(requisicao, resposta) =>{
+    resposta.sendFile('${caminhoBase}/cadastro.html')
+
+})
+    
+
+
+
 app.get('/usuarios/:id',(requisicao,resposta) => {
     const id = requisicao.params.id
     console.log('acessando dados de usuario ${id}')
